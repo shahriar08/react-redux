ac8040a64f600bc7fd84982ac2e4f52c906b34f9
AL-Shahriar <anik7744@gmail.com>
2022-05-16 01:24:03 +0600

    reducers

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+//action
 const BUY_CAKE = "BUY_CAKE"
 
 function buyCake(){
     return{
         type: BUY_CAKE,
         info: 'FIrst Redux action'
+    }
+}
+
+
+//Reducers
+//(previousState,action)=>{newState}
+
+const initialState = {
+    numOfCake: 10
+}
+
+const reducer = (state=initialState,action)=>{
+    switch(action.type){
+        case BUY_CAKE:return{
+            ...state,
+            numOfCake:state.numOfCake-1
+        }
+    default:return state
     }
 }
